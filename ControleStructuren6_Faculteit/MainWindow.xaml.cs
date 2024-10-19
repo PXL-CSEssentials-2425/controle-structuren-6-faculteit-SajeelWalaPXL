5d7eb469d179317736d7834be00abfcd28bde818
Sajeel Ahmad <12301839@student.pxl.be>
2024-10-19 22:06:02 +0200

    Faculteit 1

---
+++ ControleStructuren6_Faculteit/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControleStructuren6_Faculteit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(numberTextbox.Text, out int number);
+            int result = 1;
+            for (int i = number; i > 0; i--)
+            {
+                result *= i;
+            }
+            resultTextbox.Text = result.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            numberTextbox.Clear();
+            resultTextbox.Clear();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
